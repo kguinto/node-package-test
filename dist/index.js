d745c3ebc4a4f327edc57fdba52ff7c4dc3101d3
kguinto <klogold1@gmail.com>
2019-01-23 17:37:50 -0800

    Added husky precommit

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+
+var _Button = _interopRequireDefault(require("./Button"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+module.exports = {
+  Button: _Button.default
+};
